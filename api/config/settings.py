7a361d5b612aa61867af1b870dd823614c21087d
Matt <matt@jackalmedia.co.uk>
2024-03-18 17:31:49 +0000

    added gameservice to set and get rooms in mongo

--- api/config/settings.py
+++ api/config/settings.py
@@ -22,9 +22,19 @@ class Settings(BaseSettings):
     APP_NAME = "test_app"
     APP_VERSION = env.get("app_version")
     environment:Environment = getattr(Environment, env.get("environment"))
+    MONGO_USER = env.get("MONGO_INITDB_ROOT_USERNAME")
+    MONGO_PASS = env.get("MONGO_INITDB_ROOT_PASSWORD")
+
+
     # LOGGING
     logging_level = "DEBUG"
     logging_email_from = env.get("logging_email_from")
     logging_email_to = env.get("logging_email_to")
     logging_email_subject = env.get("logging_email_subject")
     logging_filename = f"/var/log/{APP_NAME}/{APP_NAME}.log"
+
+def get_settings():
+    config = {}
+    for key, value in Settings.schema()["properties"].items():
+        config.update({key: value["default"]})
+    return config
