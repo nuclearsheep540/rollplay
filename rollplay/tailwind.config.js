6e6c7e132d438c0020695c551855b517ac35ce3b
Matt <matt@jackalmedia.co.uk>
2025-06-11 22:18:48 +0100

    add safelist to fix border issues

--- rollplay/tailwind.config.js
+++ rollplay/tailwind.config.js
@@ -5,6 +5,26 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    // Seat color borders for PlayerCard and AdventureLog
+    'border-l-blue-500',
+    'border-l-red-500', 
+    'border-l-green-500',
+    'border-l-orange-500',
+    'border-l-purple-500',
+    'border-l-cyan-500',
+    'border-l-pink-500',
+    'border-l-lime-500',
+    // Seat color text for AdventureLog
+    'text-blue-400',
+    'text-red-400',
+    'text-green-400', 
+    'text-orange-400',
+    'text-purple-400',
+    'text-cyan-400',
+    'text-pink-400',
+    'text-lime-400',
+  ],
   theme: {
     extend: {
       backgroundImage: {
